1d72d9764a8c7b2179504cf2a7a52324a6d8b179
Shalev Shitrit <shalevshitrit15@gmail.com>
2024-06-11 12:09:59 +0300

    correct & expamle

--- example.sql
+++ example.sql
@@ -1,20 +1,13 @@
 INSERT INTO Course VALUES ('1', 'Computer Science');
 INSERT INTO Course VALUES ('2', 'Computer Science');
-INSERT INTO Course VALUES ('3', 'Computer Science');
-INSERT INTO Course VALUES ('4', 'Computer Science');
-INSERT INTO Prerequisites VALUES (1, 2);
-INSERT INTO Prerequisites VALUES (1, 4);
-INSERT INTO Prerequisites VALUES (3, 4);
-INSERT INTO Prerequisites VALUES (3, 2);
 
--- TODO check this example:
+--Optional example using Prerequisites:
 
-INSERT INTO Course VALUES ('1', 'Computer Science');
-INSERT INTO Course VALUES ('2', 'Computer Science');
-INSERT INTO Course VALUES ('3', 'Computer Science');
-INSERT INTO Prerequisites VALUES (1, 2);
-
--- TODO: Or this example:
-
-INSERT INTO Course VALUES ('1', 'Computer Science');
-INSERT INTO Course VALUES ('2', 'Computer Science');
+--INSERT INTO Course VALUES ('1', 'Computer Science');
+--INSERT INTO Course VALUES ('2', 'Computer Science');
+--INSERT INTO Course VALUES ('3', 'Computer Science');
+--INSERT INTO Course VALUES ('4', 'Computer Science');
+--INSERT INTO Prerequisites VALUES (1, 2);
+--INSERT INTO Prerequisites VALUES (1, 4);
+--INSERT INTO Prerequisites VALUES (3, 4);
+--INSERT INTO Prerequisites VALUES (3, 2);
